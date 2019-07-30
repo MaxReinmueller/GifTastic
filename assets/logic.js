dacf4c8e0c843f274841327838757253174ad1f8
Max Reinmueller <maxreinmueller@gmail.com>
2019-07-30 10:37:45 -0700

    gif buttons all work, new gifs replace old gifs.

--- assets/logic.js
+++ assets/logic.js
@@ -2,7 +2,7 @@
 var gifsButtons = ['dog', 'cat', 'horse'];
 var userPick = ('');
 // loop that creates buttons
-$(document).ready(function() {
+$(document).ready(function () {
     for (i = 0; i < gifsButtons.length; i++) {
         var topicBtn = $("<button>");
         topicBtn.addClass("gifButton");
@@ -13,41 +13,40 @@ $(document).ready(function() {
     }
 
     // retrives value from button when clicked
-$(".gifButton").on("click", function() {
-    var clicked = $(this).val();
-    userPick = clicked;
-    // userPick.push(clicked);
-    console.log(userPick);
-    getGiffs();
-  });
+    $(document).on("click", ".gifButton", function () {
+        var clicked = $(this).val();
+        userPick = clicked;
+        // userPick.push(clicked);
+        console.log(userPick);
+        getGiffs();
+    });
 
 
-// links Giphy
-function getGiffs(){
-var xhr = $.get("https://api.giphy.com/v1/gifs/search?q= "+ userPick + "&api_key=24KQv6B23tfAoFCOnUdjAn1ZKbOjZWr2&limit=10");
-xhr.done(function (res) {
-    console.log("success got data", res);
+    // links Giphy
+    function getGiffs() {
+        var xhr = $.get("https://api.giphy.com/v1/gifs/search?q= " + userPick + "&api_key=24KQv6B23tfAoFCOnUdjAn1ZKbOjZWr2&limit=10&rating=g");
+        xhr.done(function (res) {
+            console.log("success got data", res);
 
-    var giffs = res.data
-
-    for (i in giffs) {
-        $("#gifImages").append("<img src='" + giffs[i].images.original.url + "' style='height:350px; width:350px;'/>")
+            var giffs = res.data
+            $("#gifImages").empty();
+            for (i in giffs) {
+                $("#gifImages").append("<img src='" + giffs[i].images.original.url + "' style='height:350px; width:350px;'/>")
+            }
+        });
     }
-});
-}
 
 
-// adds new buttons to array
-$("#addGifs").on("click", function() {
-    var newButton = $("<button>").text($("#serchText").val());
+    // adds new buttons to array
+    $("#addGifs").on("click", function () {
+        var newButton = $("<button>").text($("#serchText").val());
         newButton.addClass("gifButton");
         newButton.attr("id", newButton.text());
         newButton.val(newButton.text());
         $(".topicButtons").append(newButton);
         gifsButtons.push(newButton);
         userPick = newButton.val();
-       console.log(userPick);
-       getGiffs();
-  });
+        console.log(userPick);
+        getGiffs();
+    });
 });
-

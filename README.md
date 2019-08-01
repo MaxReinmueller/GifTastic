d69ba296c53604b5eb00c0fc23ed1d4ba7313984
Max Reinmueller <maxreinmueller@gmail.com>
2019-08-01 09:36:25 -0700

    rating works

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # GifTastic
 
 https://maxreinmueller.github.io/GifTastic/.
+
+
+-When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+
+    
+
+-When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+-Under every gif, display its rating (PG, G, so on).
+    settings.rating = RatingType.pg
+
+-This data is provided by the GIPHY API.
+Only once you get images displaying with button presses should you move on to the next step.
+
+-Add a form to your page that takes a value from a user input box and adds it to your topics array. Then make a function call that takes each topic in the array and remakes the buttons on the page.
+
+-Deploy your assignment to Github Pages.
+
+
+*****************
+Done:
+
+Xcreate an array of strings, each one related to a topic that interests you. Save it to a variable called topics.
+
+Xmake a list to your own liking.
+
+Xtake the topics in this array and create buttons in your HTML.
+
+Xuse a loop that appends a button for each string in the array.
--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,7 @@
-// key words that generate the gifs
+// global variables
 var gifsButtons = ['dog', 'cat', 'horse'];
 var userPick = ('');
+
 // loop that creates buttons
 $(document).ready(function () {
     for (i = 0; i < gifsButtons.length; i++) {
@@ -12,16 +13,6 @@ $(document).ready(function () {
         $(".topicButtons").append(topicBtn);
     }
 
-    // retrives value from button when clicked
-    $(document).on("click", ".gifButton", function () {
-        var clicked = $(this).val();
-        userPick = clicked;
-        // userPick.push(clicked);
-        console.log(userPick);
-        getGiffs();
-    });
-
-
     // links Giphy
     function getGiffs() {
         var xhr = $.get("https://api.giphy.com/v1/gifs/search?q= " + userPick + "&api_key=24KQv6B23tfAoFCOnUdjAn1ZKbOjZWr2&limit=10&rating=g");
@@ -29,17 +20,43 @@ $(document).ready(function () {
             console.log("success got data", res);
 
             var giffs = res.data
+
             $("#gifImages").empty();
+
             for (i in giffs) {
-                $("#gifImages").append("<img src='" + giffs[i].images.original.url + "' style='height:350px; width:350px;'/>")
+                var image = ("<img src='" + giffs[i].images.original_still.url + "'class='gifTile' style='height:350px; width:350px;'/>")
+                var still = giffs[i].images.original_still.url;
+                var animated = giffs[i].images.original.url;
+                var rated = giffs[i].rating;
+                var pOne = $("<p>").text("Rating: " + rated);
+                $("#gifImages").append(image).append(pOne)
+
+                console.log(animated);
             }
+
+            // animates giffs on click
+            $(document).on("click", ".gifTile", function () {
+                console.log($(this).src);
+                // $(this).attr(animated: true)
+                // $(this).empty("<img>");
+                // console.log($(this));
+                // $(this).append("<img src='" + giffs[i].images.original.url + "'class='gifTile' style='height:350px; width:350px;'/>");
+            });
         });
     }
+
+    // retrieves value from button when clicked
+    $(document).on("click", ".gifButton", function () {
+        var clicked = $(this).val();
+        userPick = clicked;
+        console.log(userPick);
+        getGiffs();
+    });
 
 
     // adds new buttons to array
     $("#addGifs").on("click", function () {
-        var newButton = $("<button>").text($("#serchText").val());
+        var newButton = $("<button>").text($("#searchText").val());
         newButton.addClass("gifButton");
         newButton.attr("id", newButton.text());
         newButton.val(newButton.text());
@@ -49,4 +66,17 @@ $(document).ready(function () {
         console.log(userPick);
         getGiffs();
     });
+
+    // $().on("click", function () {
+
+
+    //     var state = $(this).attr('dataState')
+    //     if (state = 'still') {
+    //         $(this).attr remove still
+    //         add animate
+    //     } else {
+    //         remove animate
+    //         add still
+    //     }
+    // });
 });
